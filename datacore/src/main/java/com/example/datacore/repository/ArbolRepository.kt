ef752a30032305f9fc1bb118e69ac72eb4e1e456
JosueAlejandro <josue.garcia1@outlook.com>
2025-09-22 15:29:09 -0600

    V3 Mas reparaciones a la arquitectura. ArbolRepository.kt tiene funcion de mock up.

--- datacore/src/main/java/com/example/datacore/repository/ArbolRepository.kt
+++ datacore/src/main/java/com/example/datacore/repository/ArbolRepository.kt
@@ -1,14 +1,14 @@
 package com.example.datacore.repository
 
 import com.example.datacore.dao.ArbolDAO
-import com.example.datacore.firebase.FirebaseCitaService
+import com.example.datacore.firebase.FirebaseArbolService
 import com.example.datacore.model.Arbol
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
 class ArbolRepository(
     private val arbolDAO: ArbolDAO,
-    private val firebaseService: FirebaseCitaService
+    private val firebaseService: FirebaseArbolService
 ){
     fun getAllArbol(): Flow<List<Arbol>> = arbolDAO.getAllArbol()
 
@@ -46,6 +46,22 @@ class ArbolRepository(
             }
         } catch (_: Exception){
 
+        }
+    }
+    suspend fun insertFakeData() {
+        val fakeArbol = listOf(
+            Arbol(fecha = "2025-09-20", hora = "09:00", producto = "Arbol", precio = "100", notas = "Para decoracion"),
+            Arbol(fecha = "2025-09-21", hora = "11:00", producto = "Arbol1", precio = "150", notas = "Para cultivar"),
+            Arbol(fecha = "2025-09-22", hora = "07:00", producto = "Arbol2", precio = "110", notas = "Para sombra"),
+        )
+
+        fakeArbol.forEach {
+            arbolDAO.insertArbol(it)
+            try {
+                firebaseService.uploadArbol(it)
+            } catch (_: Exception){
+
+            }
         }
     }
 }
